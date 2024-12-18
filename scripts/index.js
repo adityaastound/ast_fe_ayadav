ab03ff05ace023b606a75216cf75e9de65f9df13
Aditya Yadav <a.yadav@astounddigital.com>
2024-12-18 17:56:43 +0530

    Lab_5: Web Accessibility, SEO, and Performance Optimization -correction

--- scripts/index.js
+++ scripts/index.js
@@ -47,19 +47,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 
-function alphaOnly(event) {
-
-  alert(event);
-
-  var key;
-
-  if (window.event) {
-    key = window.event.key;
-  } else if (e) {
-    key = e.which;
-  }
-  //var key = window.event.key || event.key;
-  alert(key.value);
-  return ((key >= 65 && key <= 90) || (key >= 95 && key <= 122));
-
-}
