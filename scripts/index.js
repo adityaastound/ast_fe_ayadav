91a64fc36802e3de889af36f1ae1c50435ea866e
Aditya Yadav <a.yadav@astounddigital.com>
2024-12-19 15:22:26 +0530

    Lab_6: Advanced JavaScript & Web APIs

--- scripts/index.js
+++ scripts/index.js
@@ -46,4 +46,61 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
+let skip = 0; 
+const limit = 10; 
+const productContainer = document.getElementById('product_container');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
 
+
+async function fetchProducts() {
+  try {
+ 
+    const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+
+    productContainer.innerHTML = '';
+
+   
+    data.products.forEach(product => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
+        <img src="${product?.images[0]}" alt="${product.title}">
+        <div class="card-body">
+          <h4 class="card-title">${product.title}</h3>
+          <p class="card-description">${product.description}</p>
+          <p class="card-price">$${product.price}</p>
+        </div>
+      `;
+      productContainer.appendChild(card);
+    });
+
+   
+    prevButton.disabled = skip === 0;
+    nextButton.disabled = data.products.length < limit;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+}
+
+
+prevButton.addEventListener('click', () => {
+  if (skip >= limit) {
+    skip -= limit;
+    fetchProducts();
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  skip += limit;
+  fetchProducts();
+});
+
+
+fetchProducts();
